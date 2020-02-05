13fa97c4a4b45208739471e546bd8903966f5001
EvgeniyBudaev <budaev.e@gmail.com>
2020-02-05 07:53:07 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+		
+	// Максимальное нечетное число из массива
+	// Arrow-function
+
+	const array = ['4', '3', '7', '10', '1'];
+
+	const sum = array
+		.map((el) => parseInt(el))
+		.reduce((total, value) => total + value)
+	
+		console.log(sum);
+
+
+
+	
+	
